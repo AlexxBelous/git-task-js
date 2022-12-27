de501b45dd30c337e6ea3423a26947fc0ad25e5d
AlexxBelous <connectionwithme1@gmail.com>
2022-12-27 08:02:23 +0200

    q

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -1,8 +1,16 @@
-/** Длина строки
+/** Строка заглавными буквами
  *
  * 1. Создайте переменную и присвойте ей любую строку
  *
- * 2. Виведите в консоль длину этой строки
+ * 2. Убедитесь что значение этой переменной НЕ является экземпляром String
+ * Используйте для этого оператор "instanceof"
+ *
+ * 3. Убедитесь что значение этой переменной имеет тип "string"
+ *
+ * 4. Создайте другую переменную и ее значением должно быть значение
+ * первой переменной заглавными буквами
+ *
+ * 5. Выведите в консоль значение второй переменной
  */
 
 
@@ -45,8 +53,38 @@
 
 
 
-// const myFavoriteSport = 'Snowboarding'
 
-// const stringLength = myFavoriteSport.length
 
-// console.log(stringLength)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const myFavoriteSeason = 'summer'
+
+// console.log(myFavoriteSeason instanceof String)
+
+// console.log(typeof myFavoriteSeason)
+
+// const seasonToUpperCase = myFavoriteSeason.toUpperCase()
+
+// console.log(seasonToUpperCase)
+
+// // СОЗДАНИЕ ЭКЗЕМПЛРЯРА String
+// const myString = new String('Bogdan')
+// console.log(myString instanceof String)
+// console.log(typeof myString)
+// console.log(myString.toUpperCase())
