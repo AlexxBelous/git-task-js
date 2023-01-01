a6f6dd21c5a70e63e7979b97ad53e755cc7eec9d
AlexxBelous <connectionwithme1@gmail.com>
2023-01-01 11:33:02 +0200

    q

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -1,8 +1,9 @@
-/** ЗАДАЧА 20 - Дата
+/** Сравнение переменных
  *
- * 1. Создайте переменную и присвойте ей текущее время в миллисекундах
+ * 1. Сравните 2 переменные "myVariable1" и "myVariable2".
+ * Выведите в консоль "true", если значение "myVariable1" меньше или равно "myVariable2".
  *
- * 2. Выведите значение в консоль
+ * 2. Преобразуйте обе переменные в числа перед сравнением.
  */
 
 
@@ -62,8 +63,35 @@
 
 
 
-// const currentDate = new Date()
 
-// const currentDateInMs = currentDate.getTime()
 
-// console.log(currentDateInMs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myVariable1 = 10
+// let myVariable2 = '5'
+
+// // Вариант 1 конвертации строки в число
+// console.log(parseInt('25'))
+// console.log(typeof parseInt('25'))
+
+// // Вариант 2 конвертации строки в число
+// console.log(+'35')
+// console.log(typeof +'35')
+
+// console.log(parseInt(myVariable1) <= parseInt(myVariable2))
+
+// myVariable1 = '20'
+// myVariable2 = 100
+
+// console.log(parseInt(myVariable1) <= parseInt(myVariable2))
