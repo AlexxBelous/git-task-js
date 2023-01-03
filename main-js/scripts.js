e6a87fe9a4b18566cc02915c6d3d6b27504856f0
AlexxBelous <connectionwithme1@gmail.com>
2023-01-03 10:10:59 +0200

    q

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -1,49 +1,15 @@
-/** ЗАДАЧА 25 - Выражение или инструкция
+/** ЗАДАЧА 26 - Что будет выведено в консоли
  *
- * Определите тип каждой конструкции JavaScript:
- *  - выражение (expression)
- *  - инструкция (statement)
- *  - выражение-инструкция (expression statement)
+ * Что будет выведено в консоли?
+ * Ответьте на вопрос без запуска кода
  */
 
-// expression statement
-15
-
-// statement
-const myObject = {
-  // Object literal is expression
-  x: 10,
-  y: true,
-}
-
-// expression statement
-myObject.z = 'abc' // <-- expression
-
-// expression statement
-delete myObject.x
-
-// statement
-let newVariable
-
-// expression statement
-newVariable = 30 + 5 // <-- expression
-
-// expression statement
-console.log(newVariable)
-/**             ^
- *          expression
- */
-
-// statement
-if (newVariable > 10) {
-  /**       ^
-   *    expression
-   */
-
-  // expression statement
-  console.log(`${newVariable} больше 10`) // <-- newVariable is expression
-  /**         _________________________
-   *                      ^
-   *                 expression
-   */
-}
+function fn() {
+    console.log('Привет из функции fn')
+  
+    return function (a) {
+      console.log(a)
+    }
+  }
+  
+  fn()(true)
