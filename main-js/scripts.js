5bbc9500067e4274242b619c87cc8f8be6fe5e83
AlexxBelous <connectionwithme1@gmail.com>
2023-01-07 11:54:45 +0200

    fix

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -51,15 +51,15 @@
 
 
 
-/**
- * Возвращает разницу двух чисел
- * @param {number} a
- * @param {number} b
- * @returns {number} Разница чисел
- */
-function diff(a, b) {
-    return a - b
-  }
+// /**
+//  * Возвращает разницу двух чисел
+//  * @param {number} a
+//  * @param {number} b
+//  * @returns {number} Разница чисел
+//  */
+// function diff(a, b) {
+//     return a - b
+//   }
   
-  const result = diff(10, 5)
-  console.log(result)
+//   const result = diff(10, 5)
+//   console.log(result)
