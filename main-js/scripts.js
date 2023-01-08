ce7ca2ba6e63535a0a6c8d455b87b02fcff56b3a
AlexxBelous <connectionwithme1@gmail.com>
2023-01-08 11:20:37 +0200

    add new question

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -1,52 +1,23 @@
-/** Длина строки
+/** Итерация по свойствам объекта
  *
- * 1. Создайте переменную и присвойте ей любую строку
+ * 1. Переберите все собственные свойства объекта
  *
- * 2. Виведите в консоль длину этой строки
+ * 2. Если имя свойства (ключ) равно "key1" или "key3",
+ * выведите значение свойства в консоль.
  */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const myFavoriteSport = 'Snowboarding'
-
-// const stringLength = myFavoriteSport.length
-
-// console.log(stringLength)
+const myObject = {
+    key1: true,
+    key5: 10,
+    key3: 'abc',
+    key4: null,
+    key10: NaN,
+  }
+  
+  const objectKeys = Object.keys(myObject)
+  
+  objectKeys.forEach((key) => {
+    if (key === 'key1' || key === 'key3') {
+      console.log(myObject[key])
+    }
+  })
