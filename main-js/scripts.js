12993ced32723f3d6b38f4dc1ba3a62db9883046
AlexxBelous <connectionwithme1@gmail.com>
2023-01-08 11:59:15 +0200

    q

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -1,16 +1,22 @@
-/** Перебор элементов массива
+/** Конвертация JSON в JavaScript объекты
  *
- * 1. Создайте функцию "cityInfo" с двумя параметрами,
- * которая возвращает строку
- * "London is at the index 0 in the myCities array"
+ * 1. Конвертируйте массив JSON объектов в массив объектов JavaScript
  *
- * 2. Переберите все элементы массива "myCities",
- * для каждого элемента вызовите функцию "cityInfo"
- * и выведите в консоль результат вызова функции
+ * 2. Выведите в консоль результирующий массив
+ *
+ * 3. Выведите в консоль "postId" второго объекта
+ *
+ * 4. Выведите в консоль "commentsQuantity" последнего объекта
  */
 
 
 
+const postsJSON = [
+    '{"postId":1355,"commentsQuantity":5}',
+    '{"postId":5131,"commentsQuantity":13}',
+    '{"postId":6134,"commentsQuantity":2}',
+    '{"postId":2351,"commentsQuantity":8}',
+  ]
 
 
 
@@ -38,9 +44,24 @@
 
 
 
-// const myCities = ['London', 'New York', 'Singapore']
 
-// const cityInfo = (city, index) =>
-//   `${city} is at the index ${index} in the myCities array`
 
-// myCities.forEach((city, index) => console.log(cityInfo(city, index)))
+// const postsJSON = [
+//     '{"postId":1355,"commentsQuantity":5}',
+//     '{"postId":5131,"commentsQuantity":13}',
+//     '{"postId":6134,"commentsQuantity":2}',
+//     '{"postId":2351,"commentsQuantity":8}',
+//   ]
+  
+//   // // OPTION 1
+//   // const postsJS = postsJSON.map((post) => JSON.parse(post))
+  
+//   // OPTION 2
+//   const postsJS = postsJSON.map(JSON.parse)
+  
+//   // console.log(postsJSON)
+//   console.log(postsJS)
+  
+//   console.log(postsJS[1].postId)
+  
+//   console.log(postsJS[postsJS.length - 1].commentsQuantity)
