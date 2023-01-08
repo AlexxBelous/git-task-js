2b0bb1f41662fbf6fca09459dae097d5f62e2b5c
AlexxBelous <connectionwithme1@gmail.com>
2023-01-08 12:24:28 +0200

    fix

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -1,17 +1,30 @@
 /** Массив объектов
  *
- * 1. Создайте массив с 3 объектами "cars"
+ * 1. В массив с 3 объектами "cars"
  *
- * 2. Каждый объект должен иметь три свойства
- *  - carBrand (строка)
- *  - price (число)
- *  - isAvailableForSale (логическое значение)
- *
- * 3. Добавьте еще один объект в массив
+ * 3. Добавьте еще один объект в массив с такими 
+ * же свойствами
  *
  * 4. Выведите результирующий массив в консоль
  */
 
+const cars = [
+  {
+    carBrand: "BMW",
+    price: 12000,
+    isAvailableForSale: true,
+  },
+  {
+    carBrand: "Honda",
+    price: 10000,
+    isAvailableForSale: false,
+  },
+  {
+    carBrand: "Toyota",
+    price: 8000,
+    isAvailableForSale: true,
+  },
+];
 
 
 
@@ -43,37 +56,12 @@
 
 
 
-
-
-
-
-
-
-
-// const cars = [
-//     {
-//       carBrand: 'BMW',
-//       price: 12000,
-//       isAvailableForSale: true,
-//     },
-//     {
-//       carBrand: 'Honda',
-//       price: 10000,
-//       isAvailableForSale: false,
-//     },
-//     {
-//       carBrand: 'Toyota',
-//       price: 8000,
-//       isAvailableForSale: true,
-//     },
-//   ]
-  
 //   const newCar = {
 //     carBrand: 'Tesla',
 //     price: 20000,
 //     isAvailableForSale: false,
 //   }
-  
+
 //   cars.push(newCar)
-  
+
 //   console.log(cars)
