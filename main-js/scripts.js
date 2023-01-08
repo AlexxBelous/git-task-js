ee568aaa349313ce8812aa8ad3242292bf2b14df
AlexxBelous <connectionwithme1@gmail.com>
2023-01-08 11:54:43 +0200

    a

--- main-js/scripts.js
+++ main-js/scripts.js
@@ -1,20 +1,74 @@
-/** Цикл "for .. in"
+/** Тернарный оператор
  *
- * Внутри цикла "for .. in" перед выводом значения свойства в консоль
- * выполните проверку того, что свойство является
- * собственным свойством объекта
+ * 1. Перепишите инструкцию "if .. else" без использования "else"
+ *
+ * 2. Перепишите содержимое функции, используя тернарный оператор
+ *
+ * 3. Замените обычную функцию на стрелочную функцию
  */
 
-const myObject = {
-    name: 'Mike',
-    age: 30,
-    city: 'London',
-  }
-  
-  Object.prototype.country = 'England'
-  
-  for (let key in myObject) {
-    if (myObject.hasOwnProperty(key)) {
-      console.log(myObject[key])
-    }
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ПОДЗАДАЧА 1
+// function isArrayEmpty(inputArray) {
+//   if (inputArray.length > 0) {
+//     return 'Массив не пустой'
+//   }
+
+//   return 'Массив пустой'
+// }
+
+// // ПОДЗАДАЧА 2
+// function isArrayEmpty(inputArray) {
+//   return inputArray.length > 0 ? 'Массив не пустой' : 'Массив пустой'
+// }
+
+// // ПОДЗАДАЧА 3 - Явный возврат результата
+// const isArrayEmpty = (inputArray) => {
+//   return inputArray.length > 0 ? 'Массив не пустой' : 'Массив пустой'
+// }
+
+// ПОДЗАДАЧА 3 - Неявный возврат результата
+const isArrayEmpty = (inputArray) =>
+  inputArray.length > 0 ? 'Массив не пустой' : 'Массив пустой'
+
+console.log(isArrayEmpty([1, 3]))
+console.log(isArrayEmpty([]))
